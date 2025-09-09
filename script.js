54b4a210bed1a0540ad1c0f6c3885e306b6d9d45
RimuruVold <144381241+RimuruVold@users.noreply.github.com>
2025-09-09 09:00:11 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function login() {
-            const user = document.getElementById('username').value;
-            const pass = document.getElementById('password').value;
-            if (user === 'adminsiji' && pass === 'Skull771') {
-                isAdmin = true;
-                toggleLogin();
-                renderTable();
-            } else {
-                alert('Invalid credentials.');
-            }
-        }
+// Firebase Config (gunakan milikmu)
+const firebaseConfig = {
+  apiKey: "AIzaSyDPc2OGZJkhxAAMHH-vkPq8SDCz-Y1kdxI",
+  authDomain: "gag-stock-ea694.firebaseapp.com",
+  projectId: "gag-stock-ea694",
+  storageBucket: "gag-stock-ea694.firebasestorage.app",
+  messagingSenderId: "469914195593",
+  appId: "1:469914195593:web:17209f759273a279ed9858"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
